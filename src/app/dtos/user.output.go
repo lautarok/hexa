40b1c5c6d7e4324212598df7499372080114d490
Lautaro <lautaro.kaza@gmail.com>
2026-01-09 04:38:32 -0300

    Fix user output dto bugs

--- src/app/dtos/user.output.go
+++ src/app/dtos/user.output.go
@@ -10,7 +10,6 @@ type UserOutputDto struct {
 	ID         uuid.UUID            `json:"id"`
 	Name       string               `json:"name"`
 	Surname    string               `json:"surname"`
-	Username   string               `json:"username"`
 	Credential *CredentialOutputDto `json:"credential"`
 	CreatedAt  time.Time            `json:"createdAt"`
 }
